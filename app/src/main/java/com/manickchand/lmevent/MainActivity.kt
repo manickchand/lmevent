57831b0f2c89734064b7d7ad9fbe9f3b8823b668
Leon <lmj@icomp.ufam.edu.br>
2019-12-14 17:26:15 -0400

    add SwipeRefresh in main

--- app/src/main/java/com/manickchand/lmevent/MainActivity.kt
+++ app/src/main/java/com/manickchand/lmevent/MainActivity.kt
@@ -4,9 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.manickchand.lmevent.adapter.EventsAdapter
 import com.manickchand.lmevent.interfaces.IserviceRetrofit
 import com.manickchand.lmevent.interfaces.RecyclerViewOnClickListenerHack
@@ -36,44 +36,50 @@ class MainActivity : AppCompatActivity(),RecyclerViewOnClickListenerHack {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //recyclerview
-        var llm = LinearLayoutManager(this)
+        val llm = LinearLayoutManager(this)
         llm.orientation = LinearLayoutManager.VERTICAL
-
         rv_events.setHasFixedSize(true)
         rv_events.layoutManager = llm
 
-        var call = this.mIserviceRetrofit.getAllEvents()
-
-       call.enqueue(object : Callback<List<Event>> {
-           override fun onResponse(call: Call<List<Event>>?, response: Response<List<Event>>?) {
-
-               mList = response!!.body()!!
-               setAdapret()
-
-               for(res in response!!.body()!!){
-                   Log.i(TAG_DEBUC,"response: "+res.image)
-               }
-           }
-           override fun onFailure(call: Call<List<Event>>?, t: Throwable?) {
-               Log.i(TAG_DEBUC,"Erro ao pegar url "+t.toString())
-           }
-       })
-
+        this.requestEvents()
+        swiperefresh.setColorSchemeResources(R.color.colorAccent)
+        swiperefresh.setOnRefreshListener{
+            this.requestEvents()
+        }
     }
 
-    fun setAdapret(){
+    fun requestEvents(){
+        swiperefresh.isRefreshing = true
+
+        var call = this.mIserviceRetrofit.getAllEvents()
+
+        call.enqueue(object : Callback<List<Event>> {
+            override fun onResponse(call: Call<List<Event>>?, response: Response<List<Event>>?) {
+
+                mList = response!!.body()!!
+                setAdapter()
+
+                swiperefresh.isRefreshing = false
+            }
+            override fun onFailure(call: Call<List<Event>>?, t: Throwable?) {
+                swiperefresh.isRefreshing = false
+                Snackbar.make(containermain, R.string.error_get_all_events, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+                Log.i(TAG_DEBUC,"[Error getAllEvents] "+t.toString())
+            }
+        })
+    }
+
+    fun setAdapter(){
         var adapter = EventsAdapter(this,mList)
         adapter.setReciclerViewOnClickListenerHack(this)
         rv_events.adapter = adapter
-
         adapter.notifyDataSetChanged()
     }
 
     override fun onClickListener(v: View?, position: Int) {
         val intent = Intent(this, EventDetailActivity::class.java)
-        intent.putExtra(KEY_EVENT, this.mList.get(position))
+        intent.putExtra(KEY_EVENT, this.mList[position])
         startActivity(intent)
-//        Toast.makeText(this, "Clicado", Toast.LENGTH_SHORT).show()
     }
 }
